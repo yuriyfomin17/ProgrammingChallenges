85751766bc5d79a5492385a0e1f6381b3d524a4e
yuriyfomin17 <fominyura2012@gmail.com>
2022-08-17 12:40:45 +0300

    commit

---
+++ src/problems/Qualified/BinaryTreeCompare/BinaryTreeCompare.java
@@ -0,0 +1,10 @@
+package problems.Qualified.BinaryTreeCompare;
+
+public class BinaryTreeCompare {
+    static class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+}
