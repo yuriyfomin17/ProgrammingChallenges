fb55ff3edf5384d7653a4c526405e87c25eb9c9e
yuriyfomin17 <fominyura2012@gmail.com>
2022-08-27 14:29:34 +0100

    commit

--- src/problems/NumberSpiral/Main.java
+++ src/problems/NumberSpiral/Main.java
@@ -1,60 +1,55 @@
 package problems.NumberSpiral;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Objects;
 
 
 public class Main {
     private static int n = 0;
+    private static Direction currentDirection = Direction.RIGHT;
+
+    private static int maxN = 0;
+    private static int idxDirection = 0;
 
     private static long nextGridNum = 1;
     private static int currRow = 0;
     private static int maxRow = 0;
+    private static int maxCol = 0;
     private static int currCol = 0;
-    private static int macCol = 0;
+    private static HashMap<Integer, Long> hashMap = new HashMap<>();
 
     enum Direction {UP, DOWN, LEFT, RIGHT}
 
     private static Direction[] oneRoundSpiral = {Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.DOWN, Direction.RIGHT, Direction.UP};
 
 
-    private static void getNumbers(List<int[]> coordinates) {
-        long[][] matrix = new long[n][n];
-        int idxDirection = 0;
-        int [] coordinateChange = getDirection(Direction.RIGHT);
+    private static void getNumbers() {
+        int [] coordinateChange = getDirection(currentDirection);
 
         while (currRow < n && currCol < n){
-
-            matrix[currRow][currCol] = nextGridNum++;
+            hashMap.put(Objects.hash(currRow, currCol), nextGridNum++);
             if (isFillLimitReached(currRow, currCol)){
-                Direction currentDirection = oneRoundSpiral[idxDirection++];
+                currentDirection = oneRoundSpiral[idxDirection++];
                 if (idxDirection == oneRoundSpiral.length) idxDirection = 0;
                 coordinateChange = getDirection(currentDirection);
             }
-
             currRow += coordinateChange[0];
             currCol += coordinateChange[1];
 
         }
-
-        coordinates.forEach(currentCoordinate ->{
-            System.out.println(matrix[currentCoordinate[0] - 1][currentCoordinate[1] - 1]);
-        });
 
 
     }
 
     public static void main(String[] args) throws IOException {
         InputStreamReader r = new InputStreamReader(System.in);
+//        InputStreamReader r = new InputStreamReader(new FileInputStream("/Users/icom/Desktop/ProgrammingChallenges/src/problems/NumberSpiral/inputData2.txt"), StandardCharsets.UTF_8);
+
         BufferedReader br = new BufferedReader(r);
         n = Integer.parseInt(br.readLine());
-        List<int[]> coordinates = new ArrayList<>();
         for (int i = 0; i < n ; i++  ){
             String string = br.readLine();
             if (string!= null && string.equals("")) continue;
@@ -64,19 +59,25 @@ public class Main {
                 resultArr[0] = Integer.parseInt(arr[0]);
                 resultArr[1] = Integer.parseInt(arr[1]);
                 n = Math.max(n, Math.max(resultArr[0], resultArr[1]));
-                coordinates.add(resultArr);
+                int hash = Objects.hash(resultArr[0] - 1, resultArr[1] - 1);
+                if (n < maxN){
+                    System.out.println(hashMap.get(hash));
+                } else {
+                    getNumbers();
+                    System.out.println(hashMap.get(hash));
+
+                }
             }
 
         }
-        getNumbers(coordinates);
     }
 
     public static boolean isFillLimitReached(int currRow, int currColumn){
         if (currRow > maxRow){
             maxRow = currRow;
             return true;
-        } else if (currColumn > macCol) {
-            macCol = currColumn;
+        } else if (currColumn > maxCol) {
+            maxCol = currColumn;
             return true;
         }
         return (currRow == 0 || currColumn == 0 || currRow == n || currColumn == n);
